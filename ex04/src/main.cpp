c15d2b0f1d3b6c22e64bae9c4eb6d58ed66c0867
Elroy Andela <eandela@f1r3s5.codam.nl>
2025-02-21 18:06:51 +0100

    add creation of .replace file in ex04

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -4,32 +4,40 @@
 
 int main(int argc, char* argv[])
 {
-	// if (argc != 2)
-	// 	std::cout << "Enter 2 arguments" << std::endl;
+	if (argc != 4)
+{
+    std::cerr << "Usage: " << argv[0] << " <filename> <search_string> <replace_string>" << std::endl;
+    return (EXIT_FAILURE);
+}
 
-	
 	std::ifstream file {argv[1]};
-	std::string	s1 {};
-	std::string s2 {"hoi"};
-	std::string s3 {"doei"};
-	std::string::size_type n {0};
-
-	while (std::getline(file, s1))
+	if (!file.is_open())
 	{
-		while (n = s1.find(s2, n) != s1.npos)
-		{
-			
-			s1.erase(n, s2.length());
-			s1.insert(n, s3);
-			n += s3.length();
-		}
-		std::cout << s1 << std::endl;
+		std::cerr << "Could not open file: " << argv[1] << std::endl;
+		return (EXIT_FAILURE);
 	}
 
-	
-	
+	std::ofstream replaceFile {std::string {argv[1]}.append(".replace"), std::ios::out | std::ios::trunc};
+	if (!replaceFile.is_open())
+	{
+		std::cerr << "Could not create file" << argv[1] << ".replace" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	std::string	line {};
+	std::string searchStr {argv[2]};
+	std::string replaceStr {argv[3]};
+	std::string::size_type pos {0};
 
-	// std::cout << n << std::endl;
-
-	// std::cout << s1 << std::endl;
+	while (std::getline(file, line))
+	{
+		pos = 0;
+		while ((pos = line.find(searchStr, pos)) != line.npos)
+		{
+			
+			line.erase(pos, searchStr.length());
+			line.insert(pos, replaceStr);
+			pos += replaceStr.length();
+		}
+		replaceFile << line << std::endl;
+	}
 }
